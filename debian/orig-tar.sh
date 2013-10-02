c4e28ca38f9bdbcdaa64bde2ca851d8b7c5888f1
Emmanuel Bourg <ebourg@apache.org>
2013-10-02 23:16:32 +0200

    Use XZ compression for the upstream tarball

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -8,12 +8,12 @@ package=`dpkg-parsechangelog | sed -n 's/^Source: //p'`
 version=$2
 upstream_version="$(echo $version | sed 's/~m/-milestone-/')"
 tarball=$3
-TAR=${package}_${version}.orig.tar.gz
+TAR=${package}_${version}.orig.tar.xz
 DIR=${package}-${upstream_version}
 
 unzip $tarball
 
-GZIP=--best tar --numeric --group 0 --owner 0 -c -v -z -f $TAR \
+XZ_OPT=--best tar --numeric --group 0 --owner 0 -c -v -J -f $TAR \
     --anchored -X debian/orig-tar.excludes $DIR
 
 rm -rf $tarball $DIR
